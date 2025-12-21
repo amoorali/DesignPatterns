aa04095dca9a44c170f0dc02910656a17f83b6dd
amoorali <amirfallahi222@gmail.com>
2025-12-21 14:30:55 +0330

    Creational - Singleton

---
+++ A - Creational/Singleton/Implementation.cs
@@ -0,0 +1,33 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        // Lazy<T>
+        private static readonly Lazy<Logger> _lazylogger
+            = new Lazy<Logger>(() => new Logger());
+
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazylogger.Value;
+            }
+        }
+
+        protected Logger()
+        {
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ A - Creational/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Singleton Pattern";
+
+            // call the property getter twice
+            var instance1 = Logger.Instance;
+            var instance2 = Logger.Instance;
+
+            if (instance1 == instance2 && instance2 == Logger.Instance)
+                Console.WriteLine("Instances are the same.");
+
+            instance1.Log($"Message from {nameof(instance1)}");
+            instance2.Log($"Message from {nameof(instance2)}");
+            Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+            Console.ReadLine();
+        }
+    }
+}
