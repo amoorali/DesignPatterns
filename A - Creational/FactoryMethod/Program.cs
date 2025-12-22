8b66dbeabb34c40bad71b23e7dbb7297e39df010
amoorali <amirfallahi222@gmail.com>
2025-12-22 12:34:46 +0330

    Creational - Factory Method

---
+++ A - Creational/FactoryMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Factory Method";
+
+            var factories = new List<DiscountFactory>
+            {
+                new CountryDiscountFactory("IR"),
+                new CodeDiscountFactory(Guid.NewGuid())
+            };
+
+            foreach (var factory in factories)
+            {
+                var discountService = factory.CreateDiscountService();
+
+                Console.WriteLine($"Percentage {discountService.DiscountPercentage} " +
+                    $"coming from {discountService}");
+            }
+        }
+    }
+}
